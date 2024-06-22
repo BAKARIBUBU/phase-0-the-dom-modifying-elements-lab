e6f63f049192bea877dd89ae8f25802e7991badc
bakari-rhoka-bubu <bakaribubu@gmail.com>
2024-06-22 08:19:52 +0300

    Dom

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// in index.js
+// Remove the <main> element with id 'main'
+document.querySelector('main#main').remove();
+
+// Create a new <h1> element and assign it to the newHeader variable
+window.newHeader = document.createElement('h1');
+window.newHeader.id = 'victory';
+window.newHeader.innerHTML = 'YOUR-NAME is the champion'; // Replace YOUR-NAME with your actual name
+
+// Add the newHeader element to the DOM
+document.body.appendChild(window.newHeader);
